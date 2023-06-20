716a8fd27a0ebedbe39ad27cc3f131ea60c2cae9
Knarkzel <knarkzel@gmail.com>
2023-06-20 18:20:29 +0200

    avoid jenkins

--- src/routes/SideBar.svelte
+++ src/routes/SideBar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import IconXml from '~icons/mdi/xml';
 </script>
 
 <div>
@@ -7,24 +8,14 @@
 
   <nav class="list-nav">
     <ul>
-      <li><a href="{base}/blog/simple">Simple</a></li>
+      <li>
+        <div class="flex justify-between">
+          <a class="w-full" href="{base}/blog/simple">Simple</a>
+          <a href="https://github.com/knarkzel/skeletonblocks/blob/master/src/routes/blog/simple/+page.svelte" target="_blank">
+            <IconXml />
+          </a>
+        </div>
+      </li>
     </ul>
   </nav>
-
-  {#if false}
-    <h3 class="h3 pb-2">CONTACT</h3>
-    <h3 class="h3 pb-2">CONTENT</h3>
-    <h3 class="h3 pb-2">CTA</h3>
-    <h3 class="h3 pb-2">ECOMMERCE</h3>
-    <h3 class="h3 pb-2">FEATURE</h3>
-    <h3 class="h3 pb-2">FOOTER</h3>
-    <h3 class="h3 pb-2">GALLERY</h3>
-    <h3 class="h3 pb-2">HEADER</h3>
-    <h3 class="h3 pb-2">HERO</h3>
-    <h3 class="h3 pb-2">PRICING</h3>
-    <h3 class="h3 pb-2">STATISTIC</h3>
-    <h3 class="h3 pb-2">STEP</h3>
-    <h3 class="h3 pb-2">TEAM</h3>
-    <h3 class="h3 pb-2">TESTIMONIAL</h3>
-  {/if}
 </div>
