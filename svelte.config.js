b9b568842defec02427c9f63b224676a294b4f54
Knarkzel <knarkzel@gmail.com>
2023-06-24 17:25:12 +0200

    hmm what

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const dev = process.argv.includes("dev");
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter({
       strict: false,
