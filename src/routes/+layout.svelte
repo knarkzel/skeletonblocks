7f5b40a238ee749e3dfc41f0a0e818c8da9383d5
Knarkzel <knarkzel@gmail.com>
2023-06-26 12:17:34 +0200

    Sleeker

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,14 +5,14 @@
 
   import { base } from "$app/paths";
   import { Drawer, drawerStore, AppShell } from "@skeletonlabs/skeleton";
-  import { LightSwitch } from '@skeletonlabs/skeleton';
+  import { LightSwitch } from "@skeletonlabs/skeleton";
   import SideBar from "./SideBar.svelte";
   import IconMenu from "~icons/mdi/menu";
-
+  import IconGitHub from "~icons/mdi/github";
 
   function openDrawer() {
     drawerStore.open({
-      width: "w-min",
+      width: "w-min"
     });
   }
 </script>
@@ -26,14 +26,17 @@
     <IconMenu class="text-2xl lg:hidden" />
   </button>
 
-  <p class="text-center">
-    Created by <a href="https://www.youtube.com/@svelterust" target="_blank">
-      <span class="anchor hidden sm:inline">@svelterust</span>
-      <img class="ml-1 inline h-8" alt="SvelteRust" src="{base}/logo.png" width="32" height="32" />
+  <div class="flex items-center gap-4">
+    <a href="https://www.youtube.com/@svelterust" target="_blank">
+      <img class="inline h-8" alt="SvelteRust" src="{base}/logo.png" width="32" height="32" />
     </a>
-  </p>
-
-  <LightSwitch />
+    
+    <a href="https://github.com/knarkzel/skeletonblocks">
+      <IconGitHub class="inline text-2xl" />
+    </a>
+    
+    <LightSwitch />
+  </div>
 </div>
 
 <AppShell slotSidebarLeft="hidden lg:block">
